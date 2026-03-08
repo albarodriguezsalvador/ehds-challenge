b7bdf478291b4c59b0d4a7a2f0f8fe3be48fcdd5
Alba Rodriguez <codebioandmore@gmail.com>
2026-03-08 21:10:22 +0100

    Add minor updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import { useState } from "react";
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 import SectionCard from '../components/SectionCard';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const [cookieVisible, setCookieVisible] = useState(false); // <--- State aquí
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -26,6 +28,8 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
+  const [cookieVisible, setCookieVisible] = useState(true); // <--- State aquí
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -34,8 +38,31 @@ export default function Home() {
       <main>
         <HomepageFeatures />
         <SectionCard />
+        {cookieVisible && (  // <-- Solo se muestra si cookieVisible = true
+          <div
+            style={{
+              background: "#222",
+              color: "#fff",
+              padding: "0.5rem",
+              textAlign: "center",
+              position: "fixed",
+              width: "100%",
+              bottom: 50,
+              zIndex: 9999,
+              fontSize: "0.6rem"
+            }}
+          >
+            Esta web usa cookies para medir visitas con Google Analytics.
+            <button
+              onClick={() => setCookieVisible(false)} // <-- Funciona ahora
+              style={{ marginLeft: "1rem", fontSize: "0.6rem" }}
+            >
+              Aceptar
+            </button>
+          </div>
+        )}
       </main>
-      
+     
     </Layout>
   );
 }
